80d5102c1a1e5d37498ce39be402cbeed6f25238
hoaihaii-el <hoaihai.el@gmail.com>
2024-09-17 22:55:40 +0700

    [WEB] config https

--- FingersFly.Client/src/app/app.component.ts
+++ FingersFly.Client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'FingerFlys.Client';
+  title = 'FingersFly.Client';
 }
